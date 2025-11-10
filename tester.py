538476d50d590a500a925dcdb9f684ac711a6632
dogsub <ehdtjq0909@gmail.com>
2025-11-10 20:53:25 +0900

    Lab11 제출

---
+++ tester.py
@@ -0,0 +1,8 @@
+from geo import utils
+
+print(utils.double_all([1, 2, 3]))
+print(utils.square_dict([1, 2, 3]))
+print(utils.pairwise_sum([1,2],[10,20]))
+print(utils.consecutive_pairs([1,2,3,4]))
+print(list(utils.square_generator(5)))
+
